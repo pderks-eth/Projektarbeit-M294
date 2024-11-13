208a9b1fac7b4148e08822ef08a69bd5f6fe9230
pderks-eth <philipp.derks@lernende.ethz.ch>
2024-11-13 11:07:31 +0100

    Add task creation and deletion functionality

--- tasks.js
+++ tasks.js
@@ -1,12 +1,21 @@
 const getButton = document.getElementById('get');
-const taskArea = document.getElementById('Tasks')
+const taskArea = document.getElementById('Tasks');
+const from = document.getElementById('addTask');
+let taskTitleFromForm = document.getElementById('title');
 const url = 'http://localhost'
 
 async function getTasks() {
+    // clear list
+    taskArea.innerHTML = '';
+
+    // get tasks
     let response = await fetch(url + '/tasks');
     let tasks = await response.json();
     console.log(tasks)
+
+    // Display tasks
     tasks.forEach((task) => {
+
         const taskDiv = document.createElement('div');
         taskDiv.classList.add('task')
 
@@ -14,7 +23,14 @@ async function getTasks() {
         taskTitle.innerHTML = task.title;
         taskDiv.appendChild(taskTitle);
 
-        taskArea.appendChild(taskDiv);
+        // add Buttons
+        const editButton = document.createElement('button');
+        editButton.innerHTML = "Edit";
+        taskDiv.appendChild(editButton);
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = "Delete";
+        deleteButton.setAttribute('onclick', `deleteTask(${task.id})`);
+        taskDiv.appendChild(deleteButton);
 
         if (task.completed) {
             taskDiv.classList.add('done');
@@ -22,14 +38,36 @@ async function getTasks() {
             taskDiv.classList.add('notDone');
         }
 
-        console.log(task.completed);
+        taskArea.appendChild(taskDiv);
     });
 }
 
+async function deleteTask(taskid) {
+    fetch(url + `/task/${taskid}`,{method: 'DELETE'});
+    update();
+}
 
+from.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
+    fetch(url + '/tasks', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            completed : false,
+            title : taskTitleFromForm.value
+        })
+    });
 
+    update();
+});
+
+function update() {
+    getTasks();
+}
 
 
 getButton.addEventListener('click', getTasks);
-
+getTasks();
